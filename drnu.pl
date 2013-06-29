6d51126b83394109bbab9dcb40c17390db0c25cc
Soren Dossing <netcom@sauber.net>
2013-06-29 23:55:28 +0900

    New resource format

--- drnu.pl
+++ drnu.pl
@@ -10,6 +10,7 @@ use warnings;
 use strict;
 use WWW::Mechanize;
 use JSON;
+use YAML::Syck;
 
 my $nupage = $ARGV[0];
 
@@ -21,10 +22,22 @@ $content =~ /resource: "(.*?)",/ and my $resource = $1;
 $mech->get( $resource );
 $content = $mech->content;
 my $dataref = decode_json $content;
+my $filename =  $dataref->{Data}[0]{Slug} . ".mp4";
+print "*** filename: $filename\n";
+my $links = $dataref->{Data}[0]{Assets}[0]{Links};
 my @resolutions =
-  sort { $b->{bitrateKbps} <=> $a->{bitrateKbps} }
-  @{ $dataref->{links} };
-my $uri = $resolutions[0]{uri}; # Highest bitrate
-$uri =~ s/cms/cms\/cms/;
-
-print "rtmpdump -r '$uri' > filename.mp4\n";
+  sort { $b->{Bitrate} <=> $a->{Bitrate} }
+  grep { $_->{Uri} =~ /^(rtmp)/ }
+  @$links;
+my $uri = $resolutions[0]{Uri}; # Highest bitrate
+print "*** uri: $uri\n";
+if ( $uri =~ /^http/ ) {
+  system "mplayer -dumpstream -playlist '$uri'";
+} elsif ( $uri =~ /^rtmp/ ) {
+  #system "rtmpdump ry '$uri' -o '$filename'";
+  my $rtmp_url = $uri;
+  $rtmp_url=~s!rtmp://vod.dr.dk/cms\/mp4:!mp4:!;
+  system "rtmpdump -r \"rtmp://vod.dr.dk/cms\" -a \"cms\" -y \"$rtmp_url\" -o \"$filename\" ";
+} else {
+  print "Unknown uri\n";
+}
